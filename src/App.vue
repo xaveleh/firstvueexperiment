f16f3b4de9f03cabb2435e2f69446120f9785647
Xava <xavadecordova@gmail.com>
2019-09-27 11:19:07 -0400

    added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,18 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
-import Header from "./components/layout/header";
-import AddTodo from "./components/AddTodo";
-import axios from 'axios';
-
+import Header from './components/layout/header';
 export default {
   name: "app",
   components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
-      
-    },
-    addTodo(newTodo) {
-      const {title, completed} = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-      .then(res => this.todos = [...this.todos, res.data])
-      
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err));
-  } 
-};
+    Header
+  }
+}
 </script>
 
 <style>
